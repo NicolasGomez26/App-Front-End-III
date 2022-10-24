247b5820a2752618975332d15c5851f7333ab797
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2022-10-24 16:33:55 -0300

    Clase 4 - Componentes y Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
+import Home from './Clase 4/Pages/Home'
+import Shop from './Clase 4/Pages/Shop'
 
 const App = () =>  {
-  let headers = ['Home', 'Shop', 'Contact', 'About']
-  let users = [
-    {id: 0, name: 'Vero', age: 49},
-    {id: 1, name: 'Juan', age: 26 },
-    {id: 2, name: 'Edu', age: 32 },
-    {id: 3, name: 'fabio', age: 21},
-  ]
   return (
     <div className="App">
-      {headers.map((header, index) => <h3 key={index}>{header}</h3>)}
-      {users.map((user) => <h2 key={user.id}> {user.name}</h2>)}
+      <Home/>
+      <Shop/>
     </div>
   )
 }
